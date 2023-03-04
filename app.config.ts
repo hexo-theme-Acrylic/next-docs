b01e680a64f229106f72962d01a5e191fce2255b
rootlex <59170489+rootlexblog@users.noreply.github.com>
2023-03-04 12:31:41 +0800

    🎉 init

---
+++ app.config.ts
@@ -0,0 +1,34 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Hexo-Theme-Acrylic',
+    titleTemplate: 'Hexo-Theme-Acrylic',
+    description: '一个美观，强大，高效的Hexo主题',
+    image: 'https://bu.dusays.com/2023/03/03/640202a7ca093.png',
+    socials: {
+      github: 'hexo-theme-Acrylic/Hexo-Theme-Acrylic-Next'
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: true
+    },
+    header: {
+      fluid: true,
+      logo: false,
+      title: 'Acrylic'
+    },
+    footer: {
+      fluid: true,
+      iconLinks: [
+        {
+          href: 'https://jq.qq.com/?_wv=1027&k=qY1BMaBT',
+          icon: 'simple-icons:tencentqq'
+        }
+      ]
+    }
+  }
+})
